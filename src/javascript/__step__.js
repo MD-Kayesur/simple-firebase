7a9e080db9dc32db2093359f339d03eb5cdd057c
MD-Kayesur <rmdkayesur@gmail.com>
2025-02-16 08:21:26 +0600

    seconed

--- src/javascript/__step__.js
+++ src/javascript/__step__.js
@@ -1,1 +1,25 @@
-// 
+/*go to firebase.google.com then go to console
+
+*create a firebase project
+
+*go to web setting for firebase app and register the app
+
+*npm install firebase
+
+*get firebase config and add that config added into project {its not a good way}
+
+*enable sign in method that you want to use : build > atuntication >get started> click
+
+on google >enable > set the email address > save
+
+*go to firebase google docs
+
+*create auth (firebase initialize auth instance)
+
+*go to the login button. add event handler and create a provide (google provider)
+
+*call the signinwithprovider (auth,googleprovider)
+
+*after successfully getting data you may set it to the usestat 
+
+*/
